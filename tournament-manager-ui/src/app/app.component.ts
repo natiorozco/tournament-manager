6a7e4735be7bef09bc08cda1883645d3e47b8042
Andrés Obando Alfaro <31627028+Andres49815@users.noreply.github.com>
2025-08-18 15:33:46 -0600

    feat: multiple stages

--- tournament-manager-ui/src/app/app.component.ts
+++ tournament-manager-ui/src/app/app.component.ts
@@ -1,24 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TournamentPageComponent } from "./components/tournament-page/tournament-page.component";
-import { BracketsManager } from 'brackets-manager';
-import { InMemoryDatabase } from 'brackets-memory-db';
-
-declare global {
-  interface Window {
-    bracketsViewer?: any | undefined;
-  }
-}
-
-const dataset16 = {
-};
-
-function getNearestPowerOfTwo(input: number): number {
-  return Math.pow(2, Math.ceil(Math.log2(input)));
-}
-
-const TOURNAMENT_ID = 'tournament-1'; // Replace with your actual tournament ID
+import dataset from './data';
 
 @Component({
   selector: 'app-root',
@@ -27,69 +11,10 @@ const TOURNAMENT_ID = 'tournament-1'; // Replace with your actual tournament ID
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
-  private db: InMemoryDatabase = new InMemoryDatabase();
-  private manager: BracketsManager;
-
+export class AppComponent {
   title = 'tournament-manager-ui';
+  public dataset = dataset.filter(x => x.roster.length > 1);
 
   constructor() {
-    this.db = new InMemoryDatabase();
-    this.manager = new BracketsManager(this.db);
-  }
-
-  async ngAfterViewInit(): Promise<void> {
-    (window as any).bracketsViewer.addLocale('en', {
-      common: {
-        'group-name-winner-bracket': '{{stage.name}}',
-        'group-name-loser-bracket': '{{stage.name}} - Repechage',
-      },
-      'origin-hint': {
-        'winner-bracket': 'WB {{round}}.{{position}}',
-        'winner-bracket-semi-final': 'WB Semi {{position}}',
-        'winner-bracket-final': 'WB Final',
-        'consolation-final': 'Semi {{position}}',
-      },
-    });
-    this.loadBrackets(dataset16).then((data: any) => {
-      console.log('Brackets data loaded:', data);
-      window.bracketsViewer.render(data, {
-        selector: '#brackets',
-      })
-    });
-  }
-
-  private async loadBrackets(dataset: any): Promise<any> {
-    this.db.setData({
-      participant: dataset.roster.map((player: any) => ({
-        ...player,
-        tournament_id: TOURNAMENT_ID,
-      })),
-      stage: [],
-      group: [],
-      round: [],
-      match: [],
-      match_game: [],
-    });
-
-    await this.manager.create.stage({
-      name: dataset.title,
-      tournamentId: TOURNAMENT_ID,
-      type: dataset.type,
-      seeding: dataset.roster.map((player: any) => player.name),
-      settings: {
-        seedOrdering: ['natural'],
-        size: getNearestPowerOfTwo(dataset.roster.length),
-      },
-    });
-
-    const data = await this.manager.get.stageData(0);
-
-    return {
-      stages: data.stage,
-      matches: data.match,
-      matchGames: data.match_game,
-      participants: data.participant,
-    };
   }
 }
